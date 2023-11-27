3229e3c3a7ed15f94762c3bba181cc6f6f3daf2d
tavinchi <120009763+tavinchi@users.noreply.github.com>
2023-11-27 11:25:09 +0900

    fix

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-#!C:\Users\hp\AppData\Local\Programs\Python\Python312\python.exe
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#print("content-Type: text/plain\n")
 
 import cgi
 import sys
@@ -18,40 +19,20 @@ def check_word_match(text, target_word):
     else:
         return False
 
-
- 
 # 日本語を受信時にエラーにならないようにする為に必要。
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
- 
-form = cgi.FieldStorage()
-# 入力チェック（必要な変数が送信されていない場合はエラー。）
-#if 'inputData' not in form:
-#    print('Content-type: text/html; charset=UTF-8')
-#    print('')
-#    result="inputdata フィールドが送信されていません。"
-#    sys.exit()
- 
-# your_name の値を取得して変数にセット。
-# 値が入力されていない場合は「匿名」を設定。
+
+storage = cgi.FieldStorage()
 
 # 文中に目標単語が含まれているか確認
-if check_word_match(form.getfirst('inputData','匿名'),"モデル"):
-    #print('Content-type: text/html; charset=UTF-8')
-    #print('')
+if check_word_match(storage.getfirst('data','匿名'),"モデル"):
     result="モデル相談会は作ったモデルで疑問に思っているところなどを聞く場所です。"
-    #print("モデル相談会は作ったモデルで疑問に思っているところなどを聞く場所です。")
-elif check_word_match(form.getfirst('inputData','匿名'), "ロボコン"):
-    #print('Content-type: text/html; charset=UTF-8')
-    #print('')
+elif check_word_match(storage.getfirst('data','匿名'), "ロボ"):
     result="ETロボコンとは初心者エンジニアを対象としたロボットコンテストです。"
-    #print("ETロボコンとは初心者エンジニアを対象としたロボットコンテストです。")
 else:
-    #print('Content-type: text/html; charset=UTF-8')
-    #print('')
     result="一致しませんでした"
-    #print("一致しませんでした")
 
-#結果をHTMLに返す
+#結果をhtmlに返す
 print('Content-type: text/html; charset=UTF-8')
 print('')
 print(f"{result}")
